af4e3e8125696e1b84c766fe59362df702a44421
KKe-coder <carumag726@gmail.com>
2020-11-04 12:19:28 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f096b1f73318a29a446a24ac6ba2c4de5013d4acf67b6653dbde2442a850b3cc2319ed5f0424cc230c42a0833e6dc9eba778312dc801f2456ddd51b4aa3ac3d3'
